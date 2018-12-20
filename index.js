44ef37b35e12d1be01895d228463ff1e92e991b1
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-12-20 01:39:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(customers_block) {
+  const hqBlock = 42
+  let distFromHQ = 0
+  if (customers_block > hqBlock)  {
+    distFromHQ = customers_block - hqBlock;
+  }
+  else if (customers_block < hqBlock) {
+    distFromHQ = hqBlock - customers_block
+  }
+  else {
+    distFromHQ = 0
+  }
+  distFromHQ 
+}
+
+distanceFromHqInFeet()
+
+distanceTravelledInFeet()
+
+calculatesFarePrice(start, destination)
+
